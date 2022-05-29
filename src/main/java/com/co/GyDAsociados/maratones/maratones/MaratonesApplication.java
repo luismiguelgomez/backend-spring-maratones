9de11644dda7f2b3459fd61d703ae08d44d0e2b9
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2022-05-29 11:36:01 -0500

    inicio de proyecto

---
+++ src/main/java/com/co/GyDAsociados/maratones/maratones/MaratonesApplication.java
@@ -0,0 +1,13 @@
+package com.co.GyDAsociados.maratones.maratones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaratonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaratonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/GyDAsociados/maratones/maratones/MaratonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.GyDAsociados.maratones.maratones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaratonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
